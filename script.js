e90cb0fc45dc23fb167adbe6689b2473fe05449b
NIKHIL SHARMA <77260834+Nikhil062003@users.noreply.github.com>
2023-03-06 11:41:32 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+var g=document.getElementById("btn");
+  
+ g.addEventListener("click", function(){
+    var j=document.getElementById("Third");
+    j.innerHTML="Changed Text3";
+     var g=document.getElementById("First");
+    g.innerHTML="Changed Text1";
+      var l=document.getElementById("Second");
+    l.innerHTML="Changed Text2";
+
+  });
